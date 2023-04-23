3cb75d9f675db88b5ab8eb9fe718bb6faaa079aa
su_song <su_song@glifemail.com>
2023-04-23 15:31:44 +0800

    导出face_library_api.dart

--- lib/flutter_face_library.dart
+++ lib/flutter_face_library.dart
@@ -1,4 +1,5 @@
 library flutter_face_library;
 
-export 'src/flutter_face_library_plugin.dart';
 export 'src/face_api.dart';
+export 'src/face_library_api.dart';
+export 'src/flutter_face_library_plugin.dart';
